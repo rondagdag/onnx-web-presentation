b3351471f4fd514a898e47d275cf994d1230504e
Ron Dagdag <rlyle78@gmail.com>
2022-09-15 08:12:40 -0500

    updates

--- node/index.js
+++ node/index.js
@@ -11,20 +11,20 @@ async function main() {
         const session = await ort.InferenceSession.create('./model.onnx');
 
         // prepare inputs. a tensor need its corresponding TypedArray as data
-        const yearsExperience = Float32Array.from([10]);
+        const yearsExperience = Float32Array.from([11.5]);
         const salary = Float32Array.from([0]);
         const tensorYears = new ort.Tensor('float32', yearsExperience, [1, 1]);
         const tensorSalary = new ort.Tensor('float32', salary, [1, 1]);
 
         // prepare feeds. use model input names as keys.
         const feeds = { yearsExperience: tensorYears, salary: tensorSalary };
-
+        console.log(`feeds: ${JSON.stringify(feeds, null, " ")}`);
         // feed inputs and run
         const results = await session.run(feeds);
 
         // read from results
         const dataC = results["Score.output"].data;
-        console.log(`data of result tensor: ${dataC}`);
+        console.log(`data of results["Score.output"]: ${dataC}`);
 
     } catch (e) {
         console.error(`failed to inference ONNX model: ${e}.`);
